7b666ad9d1e53b19415daf47a5644527679df4dd
cyber_kid <amyrgorod@gmail.com>
2017-05-22 19:39:58 +0300

    add basic styles to login form

--- src/main/java/com/home/client/loginpage/LoginViewImpl.java
+++ src/main/java/com/home/client/loginpage/LoginViewImpl.java
@@ -4,13 +4,12 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.PasswordTextBox;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.*;
 import com.gwtplatform.mvp.client.HasUiHandlers;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import com.home.client.resources.AppResources;
+import com.home.client.resources.LoginPageStyle;
 
 import javax.inject.Inject;
 
